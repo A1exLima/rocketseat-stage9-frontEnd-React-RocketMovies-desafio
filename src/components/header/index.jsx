a689c0b9ae3fe06c337afc86d2219a11b568f05d
Alex Lima <alex.lima_dev@outlook.com>
2023-07-21 19:22:07 -0300

    Teste modificação netlify

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -36,7 +36,7 @@ export function Header(){
           type="button" 
           onClick={handleHome}
         >
-          RocketMovies
+          RocketMovies teste
         </button>
       </Brand>
 
